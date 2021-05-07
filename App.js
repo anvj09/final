a6b3daee157751793ef9dcd64813704d1b1b2f2a
anvj09 <65511481+anvj09@users.noreply.github.com>
2021-05-07 16:47:40 -0400

    2nd

---
+++ App.js
@@ -0,0 +1,27 @@
+var clicked = false;
+
+$("a").click(function(){
+    var idVal = $(this).attr("href");
+    $("html,body").animate({
+        'scrollTop':$(idVal+"Section").offset().top
+    }, 500);
+});
+
+$(".dropDownMenu").click(function(){
+    $(".navWrapper").toggleClass("toggle");
+    clicked=!clicked;
+});
+
+$(window).resize(function() {
+    if (window.innerWidth > 600&&(".navWrapper").hasClass("toggle")){
+        $(".navWrapper").removeClass("toggle")
+    }else if(window.innerWidth<600&&clicked){
+        $(".navWrapper").addClass("toggle");
+    }
+});
+
+
+function myFunction() {
+    let popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
